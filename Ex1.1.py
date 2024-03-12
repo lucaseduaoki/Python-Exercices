b38393c4634d9dae5f8eacd4ddeacbb499452dd4
Lucas Aoki <161040368+lucaseduaoki@users.noreply.github.com>
2024-03-11 22:25:03 -0300

    Create Ex1.1.py

---
+++ Ex1.1.py
@@ -0,0 +1,5 @@
+comp = float(input("Qual o comprimento da parede: "))
+larg = float(input("Qual a largura da parede: "))
+area = comp * larg
+tinta = area/2
+print(f'A área dessa parede é {area} e é necessário utilizar {tinta} litros de tinta')
